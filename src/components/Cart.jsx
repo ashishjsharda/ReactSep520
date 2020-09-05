ee91b793b2a09d5a68d79d3519934b60b929bf94
Ashish Sharda <ashishjsharda@gmail.com>
2020-09-05 17:37:19 -0400

    Add all my files

---
+++ src/components/Cart.jsx
@@ -0,0 +1,14 @@
+import React, {Component, createContext} from 'react'
+class Cart extends Component{
+    render() {
+        return(
+            <div>
+                <h1> This is coming from components</h1>
+                <p> Example using components</p>
+            </div>
+        )
+    }
+
+}
+
+export default Cart;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Cart from './components/Cart'
+
+
+ReactDOM.render(
+  <Cart />,
+  document.getElementById('root')
+);
+
